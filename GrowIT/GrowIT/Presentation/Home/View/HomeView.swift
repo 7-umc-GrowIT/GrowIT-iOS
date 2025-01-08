2186fb8590b7a271255975c9e62df579760a45a6
helljh <junho308917@naver.com>
2025-01-08 22:59:38 +0900

    [feat] ✨홈화면 폴더생성

---
+++ GrowIT/GrowIT/Presentation/Home/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  GrowIT
+//
+//  Created by 허준호 on 1/7/25.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
