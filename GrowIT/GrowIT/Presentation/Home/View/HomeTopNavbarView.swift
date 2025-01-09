55a7b03e338f4882201e5ed7de4e4a14282da12f
helljh <junho308917@naver.com>
2025-01-09 16:00:28 +0900

    [docs]🎨SUITE 폰트 추가

---
+++ GrowIT/GrowIT/Presentation/Home/View/HomeTopNavbarView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTopNavbarView.swift
+//  GrowIT
+//
+//  Created by 허준호 on 1/8/25.
+//
+
+import UIKit
+
+class HomeTopNavbarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
