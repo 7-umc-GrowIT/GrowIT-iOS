8b29626a6c124fc8b2fb9eb66f7d9f69a12eb1b6
이수현 <164829006+JustinLee02@users.noreply.github.com>
2025-01-16 19:57:38 +0900

    #Feat: 음성 일기 작성 파일 생성

--- GrowIT/GrowIT/Presentation/Diary/ViewController/TextDiaryVCs/TextDiaryRecommendChallengeViewController.swift
+++ GrowIT/GrowIT/Presentation/Diary/ViewController/TextDiaryVCs/TextDiaryRecommendChallengeViewController.swift
@@ -83,11 +83,12 @@ class TextDiaryRecommendChallengeViewController: UIViewController {
          }
          present(nextVC, animated: true, completion: nil)
          */
-        
-        
-        let nextVC = TextDiaryEndViewController()
-        navigationController?.pushViewController(nextVC, animated: true)
-        
+        if buttonCount == 0 {
+            Toast.show(image: UIImage(named: "toast_Icon") ?? UIImage(), message: "한 개 이상의 챌린지를 선택해 주세요", font: .heading3SemiBold(), in: self.view)
+        } else {
+            let nextVC = TextDiaryEndViewController()
+            navigationController?.pushViewController(nextVC, animated: true)
+        }
     }
     
     @objc func buttonTapped(_ sender: CircleCheckButton) {
