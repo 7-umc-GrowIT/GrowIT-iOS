44e83ebbec592ece53ee6bad06f9284a438a640a
이수현 <164829006+JustinLee02@users.noreply.github.com>
2025-01-19 02:54:03 +0900

    Chore: Moya 라이브러리 추가

---
+++ GrowIT/GrowIT/Resources/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  GrowIT
+//
+//  Created by 이수현 on 1/19/25.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct API {
+        static let baseURL = ""
+        static let diaryURL = "\(baseURL)/diaries"
+    }
+}
