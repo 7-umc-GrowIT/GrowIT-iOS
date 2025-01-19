2d6b3d652c37cad195d209b6706346cc96be906c
이수현 <164829006+JustinLee02@users.noreply.github.com>
2025-01-19 15:54:34 +0900

    Feat: 음성 일기 음성 일기 요약 뷰 및 이탈 화면 구현

--- GrowIT/GrowIT/Presentation/Diary/ViewController/VoiceDiaryVCs/VoiceDiarySummaryViewController.swift
+++ GrowIT/GrowIT/Presentation/Diary/ViewController/VoiceDiaryVCs/VoiceDiarySummaryViewController.swift
@@ -8,22 +8,77 @@
 import UIKit
 
 class VoiceDiarySummaryViewController: ViewController {
-
+    
+    // MARK: Properties
+    let voiceDiarySummaryView = VoiceDiarySummaryView()
+    let navigationBarManager = NavigationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
+        setupUI()
+        setupActions()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: Setup Navigation Bar
+    private func setupNavigationBar() {
+        navigationBarManager.addBackButton(
+            to: navigationItem,
+            target: self,
+            action: #selector(prevVC),
+            tintColor: .white
+        )
+        
+        navigationBarManager.setTitle(
+            to: navigationItem,
+            title: "",
+            textColor: .black
+        )
     }
-    */
-
+    
+    // MARK: Setup UI
+    private func setupUI() {
+        view.addSubview(voiceDiarySummaryView)
+        voiceDiarySummaryView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: Setup Actions
+    private func setupActions() {
+        voiceDiarySummaryView.saveButton.addTarget(self, action: #selector(nextVC), for: .touchUpInside)
+        
+    }
+    
+    //MARK: - @objc methods
+    @objc func prevVC() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func nextVC() {
+        let nextVC = VoiceDiaryRecommendChallengeViewController()
+        navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    // 일기 요약 이탈 시 오류 modal 뷰
+    @objc func labelTapped() {
+        let nextVC = VoiceDiarySummaryErrorViewController()
+        nextVC.modalPresentationStyle = .pageSheet
+        
+        if let sheet = nextVC.sheetPresentationController {
+            //지원할 크기 지정
+            if #available(iOS 16.0, *) {
+                sheet.detents = [
+                    .custom{ context in
+                        0.37 * context.maximumDetentValue
+                    }
+                ]
+            } else {
+                sheet.detents = [.medium()]
+            }
+            sheet.prefersGrabberVisible = true
+        }
+        present(nextVC, animated: true, completion: nil)
+    }
+    
 }
