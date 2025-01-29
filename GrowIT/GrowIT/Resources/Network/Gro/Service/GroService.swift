1a1873fbc3940721bff6f51942582d394245bee4
hyunm1n-o <hmoh0703@naver.com>
2025-01-29 23:41:05 +0900

    Feat: Gro API 메서드  구현

--- GrowIT/GrowIT/Resources/Network/Gro/Service/GroService.swift
+++ GrowIT/GrowIT/Resources/Network/Gro/Service/GroService.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import Moya
+
+final class GroService: NetworkManager {
+    typealias Endpoint = GroEndpoint
+    
+    let provider: MoyaProvider<Endpoint>
+    
+    init(provider: MoyaProvider<GroEndpoint>? = nil) {
+        let plugins: [PluginType] = [
+            NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))
+        ]
+        
+        self.provider = provider ?? MoyaProvider<GroEndpoint>(plugins: plugins)
+    }
+    
+    // 그로와 착용아이템 이미지 조회 API
+    func getGroImage(completion: @escaping (Result<GroGetResponseDTO, NetworkError>) -> Void) {
+        request(
+            target: .getGroImage,
+            decodingType: GroGetResponseDTO.self,
+            completion: completion
+        )
+    }
+    
+    // 그로 캐릭터 생성 API
+    func postGroCreate(data: GroRequestDTO, completion: @escaping (Result<GroPostResponseDTO, NetworkError>) -> Void) {
+        request(
+            target: .postGroCreate(data: data),
+            decodingType: GroPostResponseDTO.self,
+            completion: completion
+        )
+    }
+}
