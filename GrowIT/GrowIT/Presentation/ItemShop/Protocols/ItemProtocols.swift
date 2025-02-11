76e4e87f0d54438290b770d23f443b65a8364bb2
hyunm1n-o <hmoh0703@naver.com>
2025-02-12 01:22:38 +0900

    Feat: 마이아이템 진입 시 착용중인 아이템 '착용중' 으로 업데이트

--- GrowIT/GrowIT/Presentation/ItemShop/Protocols/ItemProtocols.swift
+++ GrowIT/GrowIT/Presentation/ItemShop/Protocols/ItemProtocols.swift
@@ -9,5 +9,6 @@ import Foundation
 
 // 아이템셀 선택 했을 때
 protocol ItemListDelegate: AnyObject {
+    var categoryToEquippedId: [String: Int] { get }
     func didSelectItem(_ isPurchased: Bool, selectedItem: ItemList?)
 }
